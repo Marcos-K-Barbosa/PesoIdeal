5fc23dc73c531c95c1798950161c408909e55a14
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-09 22:42:33 -0300

    Finalizado.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿String Resposta;
+String Gênero;
+Double Altura, PesoIdeal;
+Console.Clear();
+Console.Write("Olá, boas vindas! Você gostaria de calcular o peso que seria ideal para você, segundo suas proporções? ( Sim ou Não ): ");
+Resposta = (Console.ReadLine())!;
+
+if (Resposta == "Não")
+{
+
+}
+
+else if (Resposta == "Sim")
+{
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("Ok! Então vamos lá. Primeiro insira sua altura ( Em metros ): ");
+Altura = Convert.ToDouble (Console.ReadLine());
+
+Console.Write("Agora insira seu gênero ( M para Masculino e F para Feminino): ");
+Gênero = (Console.ReadLine())!;
+
+
+if (Gênero=="M")
+
+{
+Console.Write("Calculando seu peso...");
+Thread.Sleep(2000);
+PesoIdeal = Convert.ToDouble(0);
+PesoIdeal = (Altura * 72.7) - 58.0;
+Console.WriteLine($"\nSeu peso ideal é: {PesoIdeal:N1}kg");
+}
+
+else if (Gênero=="F")
+{
+Console.Write("Calculando seu peso...");
+Thread.Sleep(2000);
+PesoIdeal = Convert.ToDouble(0);
+PesoIdeal = (Altura * 62.1) - 44.7;
+Console.WriteLine($"\nSeu peso ideal é: {PesoIdeal:N1}kg");
+}
+}
